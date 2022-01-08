e228683808a23795321f84a7caf61f684d31c181
vinayak ushakola <vinayak.ushakola@gmail.com>
2022-01-08 10:50:45 +0530

    Starting the project with a simple message printing on console

---
+++ MyUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============ My Utility ============");
+        }
+    }
+}
